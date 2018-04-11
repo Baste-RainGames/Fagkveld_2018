6096dcabc56106f8f2acd2f8ceac4eb5fbc6e911
Baste Nesse Buanes <bnbuanes@gmail.com>
2018-04-11 23:14:27 +0200

    created placeholders for hp display task

---
+++ UnityProject/Assets/HitpointsDisplay.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitpointsDisplay : MonoBehaviour {
+
+    //Task 1: What does this display? How do we get at that data
+
+    void Start () {
+        //Taks 2: Initialize to show correct amount of HP
+    }
+
+    void Update () {
+        //Task 3: Handle target HP changing.
+    }
+}
